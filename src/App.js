a254aaaab963f139e12372a01c21f4cde067bc35
arnas.blaskys@gmail.com <61343729+arnas69@users.noreply.github.com>
2020-03-03 12:11:45 +0200

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Privet Andrei!!!!!!!! Edit <code>src/App.js</code> and save to reload.
+          Pro DevOps skills yaeaah!
         </p>
         <a
           className="App-link"
